f6d67b8a052be27f518ca27cb38c0b950e62896f
matias9486 <matias9486mdp@gmail.com>
2023-11-27 14:47:55 -0300

    creacion componente listado empleados

--- recursos-humanos-react/src/App.js
+++ recursos-humanos-react/src/App.js
@@ -1,8 +1,8 @@
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+
 function App() {
   return (
-    <div class="container text-center">
-      <h3>Sistema de Recursos Humanos</h3>      
-    </div>
+    <ListadoEmpleados/>
   );
 }
 
