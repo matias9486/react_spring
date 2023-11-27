fddb03716c53f048db991435d273f62691465c31
matias9486 <matias9486mdp@gmail.com>
2023-11-27 13:14:31 -0300

    Creación proyecto con spring

---
+++ recursos-humanos-Spring/src/test/java/com/practica/rrhh/RrhhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.rrhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrhhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
