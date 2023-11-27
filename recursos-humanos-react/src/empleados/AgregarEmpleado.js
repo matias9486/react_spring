1b3a1a167a18749378b7c49ff2b7284814729bd5
matias9486 <matias9486mdp@gmail.com>
2023-11-27 16:55:50 -0300

    Finalizado componente agregarEmpleado. Uso de useState de react y useNavigate de react-router-dom

--- recursos-humanos-react/src/empleados/AgregarEmpleado.js
+++ recursos-humanos-react/src/empleados/AgregarEmpleado.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function AgregarEmpleado() {
+    let navegacion = useNavigate();
+
+    const [empleado, setEmpleado] = useState({
+        nombre:"",
+        departamento:"",
+        sueldo:""
+    })
+
+    const{ nombre, departamento, sueldo } = empleado;
+
+    const onInputChange = (e) => {
+        //spread operator ... para expandir los atributos
+        setEmpleado({...empleado, [e.target.name]: e.target.value})
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()        ;
+        const urlBase = "http://localhost:8080/rh-app/empleados"
+        await axios.post(urlBase, empleado);
+        //redirigimos a la pagina de inicio
+        navegacion('/');
+    }
+
+
     return (
         <div className='container'>
             <div className='container' style={{ margin: "30px" }}>
                 <h3 className='text-center'>Agregar Empleado</h3>
 
-                <form>
+                <form onSubmit={(e) => onSubmit(e)}>
                     <div className="mb-3">
                         <label htmlfor="nombre" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" id="nombre" name='nombre' required ={true}/>                    
+                        <input type="text" className="form-control" id="nombre" name='nombre' required ={true}
+                        value={nombre} onChange={(e) => onInputChange(e)}/>                    
+
                     </div>
 
                     <div className="mb-3">
                         <label htmlfor="departamento" className="form-label">Departamento</label>
-                        <input type="text" className="form-control" id="departamento" name='departamento' required ={true}/>
+                        <input type="text" className="form-control" id="departamento" name='departamento' required ={true}
+                        value={departamento} onChange={(e) => onInputChange(e)}/>                    
                     </div>
 
                     <div className="mb-3">
                         <label htmlfor="sueldo" className="form-label">Sueldo</label>
-                        <input type="number" step="any"  className="form-control" id="sueldo" name='sueldo' required ={true}/>
+                        <input type="number" step="any"  className="form-control" id="sueldo" name='sueldo' required ={true}
+                        value={sueldo} onChange={(e) => onInputChange(e)}/>                    
                     </div>
                     
                     <div className='text-center'>
