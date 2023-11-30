e4ac59f868679c6d7ed45365d5bc0ab239efa7fc
matias9486 <matias9486mdp@gmail.com>
2023-11-30 15:01:40 -0300

    Se agrego metodo para obtener empleado por Id

--- recursos-humanos-Spring/src/main/java/com/practica/rrhh/controlador/EmpleadoControlador.java
+++ recursos-humanos-Spring/src/main/java/com/practica/rrhh/controlador/EmpleadoControlador.java
@@ -1,10 +1,12 @@
 package com.practica.rrhh.controlador;
 
+import com.practica.rrhh.excepcion.RecursoNoEncontradoExcepcion;
 import com.practica.rrhh.modelo.Empleado;
 import com.practica.rrhh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +33,15 @@ public class EmpleadoControlador {
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
     }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if(empleado == null)
+            throw new RecursoNoEncontradoExcepcion("No se encontró el id: " + id);
+        return ResponseEntity.ok(empleado);
+
+
+    }
+    
 }
