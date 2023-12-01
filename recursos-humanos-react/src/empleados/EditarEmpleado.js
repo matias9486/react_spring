3b56bf4f3a970028cb9b2a6ce2c95c861d6d23d8
matias9486 <matias9486mdp@gmail.com>
2023-12-01 13:59:26 -0300

    Agregado componente EditarEmpleado. Uso de useParams de react-router-dom

---
+++ recursos-humanos-react/src/empleados/EditarEmpleado.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+
+export default function EditarEmpleado() {
+    const urlBase = "http://localhost:8080/rh-app/empleados";
+    let navegacion = useNavigate();
+    
+    const {id} = useParams();
+
+    const [empleado, setEmpleado] = useState({
+        nombre:"",
+        departamento:"",
+        sueldo:""
+    })
+
+    const{ nombre, departamento, sueldo } = empleado;
+
+    useEffect( () => {
+        cargarEmpleado();
+    }, [])
+
+    const cargarEmpleado = async () => {
+        const resultado = await axios.get( `${urlBase}/${id}`)
+        setEmpleado(resultado.data)
+    }
+
+    const onInputChange = (e) => {
+        //spread operator ... para expandir los atributos
+        setEmpleado({...empleado, [e.target.name]: e.target.value})
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()        ;
+        
+        await axios.put(`${urlBase}/${id}`, empleado);
+        //redirigimos a la pagina de inicio
+        navegacion('/');
+    }
+
+
+    return (
+        <div className='container'>
+            <div className='container' style={{ margin: "30px" }}>
+                <h3 className='text-center'>Editar Empleado</h3>
+
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className="mb-3">
+                        <label htmlfor="nombre" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" id="nombre" name='nombre' required ={true}
+                        value={nombre} onChange={(e) => onInputChange(e)}/>                    
+
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlfor="departamento" className="form-label">Departamento</label>
+                        <input type="text" className="form-control" id="departamento" name='departamento' required ={true}
+                        value={departamento} onChange={(e) => onInputChange(e)}/>                    
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlfor="sueldo" className="form-label">Sueldo</label>
+                        <input type="number" step="any"  className="form-control" id="sueldo" name='sueldo' required ={true}
+                        value={sueldo} onChange={(e) => onInputChange(e)}/>                    
+                    </div>
+                    
+                    <div className='text-center'>
+                        <button type="submit" className="btn btn-warning btn-sm me-3">Guardar</button>
+                        <a href='/' className='btn btn-danger btn-sm'>Regresar</a>
+                    </div>
+
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
