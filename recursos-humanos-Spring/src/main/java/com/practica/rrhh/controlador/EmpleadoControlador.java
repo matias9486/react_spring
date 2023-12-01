4a8d393ec84087a2b803d0149341fa1de721595b
matias9486 <matias9486mdp@gmail.com>
2023-12-01 14:24:10 -0300

    Agregado metodo eliminar empleado

--- recursos-humanos-Spring/src/main/java/com/practica/rrhh/controlador/EmpleadoControlador.java
+++ recursos-humanos-Spring/src/main/java/com/practica/rrhh/controlador/EmpleadoControlador.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 //http://localhost:8080/rh-app/
@@ -56,5 +58,19 @@ public class EmpleadoControlador {
 
         return ResponseEntity.ok(empleado);
     }
-    
+
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(
+            @PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if( empleado == null)
+            throw new RecursoNoEncontradoExcepcion("El Id recibido no existe");
+
+        empleadoServicio.eliminarEmpleado(empleado);
+        //Json {"eliminado":"true"}
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("Eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
+
+    }
 }
